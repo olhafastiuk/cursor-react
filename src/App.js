d0803339f111f9ee22914bf05651bf736d97e3a4
Olha Fastiuk <olhafastiuk@gmail.com>
2021-08-03 01:51:15 +0300

    Upgrade styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import React from "react";
@@ -8,8 +7,18 @@ import Users from "./Users";
 function App() {
   return (
     <div className="tweets">
-      {Users.map(user => (
-        <Tweet img={user.img} name={user.name} nickname={user.nickname} date={user.date} post={user.post} img_post={user.img_post} comment={user.comment} repost={user.repost} like={user.like} />
+      {Users.map((user) => (
+        <Tweet
+          img={user.img}
+          name={user.name}
+          nickname={user.nickname}
+          date={user.date}
+          post={user.post}
+          img_post={user.img_post}
+          comment={user.comment}
+          repost={user.repost}
+          like={user.like}
+        />
       ))}
     </div>
   );
--- src/Users.js
+++ src/Users.js
@@ -1,28 +1,30 @@
 const Users = [
   {
-    img:"https://pbs.twimg.com/profile_images/1321163587679784960/0ZxKlEKB_400x400.jpg",
+    img: "https://pbs.twimg.com/profile_images/1321163587679784960/0ZxKlEKB_400x400.jpg",
     name: "NASA",
     nickname: "@NASA",
     date: "Aug 1",
     post: "💫 It’s time for one of the biggest meteor showers of the year – the Perseids! Here’s how you can watch them at their peak in mid-August:",
-    img_post: "https://pbs.twimg.com/media/E7p4jnGXIAM07gg?format=jpg&name=large",
+    img_post:
+      "https://pbs.twimg.com/media/E7p4jnGXIAM07gg?format=jpg&name=large",
     comment: "15",
     repost: "93",
     like: "668",
   },
   {
-    img:"https://pbs.twimg.com/profile_images/1229760579650363392/LSX8S-Dq_400x400.jpg",
+    img: "https://pbs.twimg.com/profile_images/1229760579650363392/LSX8S-Dq_400x400.jpg",
     name: "Black Hole",
     nickname: "@konstructivizm",
     date: "Aug 1",
     post: "One of the most detailed picture ever taken from Jupiter so far. (NASA)",
-    img_post: "https://pbs.twimg.com/media/E7s53QaWUAQYQDh?format=jpg&name=900x900",
+    img_post:
+      "https://pbs.twimg.com/media/E7s53QaWUAQYQDh?format=jpg&name=900x900",
     comment: "10",
     repost: "17",
     like: "220",
   },
   {
-    img:"https://pbs.twimg.com/profile_images/1222552890293788672/YgfaU7dI_400x400.jpg",
+    img: "https://pbs.twimg.com/profile_images/1222552890293788672/YgfaU7dI_400x400.jpg",
     name: "Universal-Sci",
     nickname: "@universal_sci",
     date: "Aug 2",
@@ -31,7 +33,8 @@ const Users = [
     SpaceX Cargo Dragon resupply ship approaching the space station
     
     (Credit: NASA Johnson)`,
-    img_post: "https://pbs.twimg.com/media/E7vgKGhWYAENIm3?format=jpg&name=4096x4096",
+    img_post:
+      "https://pbs.twimg.com/media/E7vgKGhWYAENIm3?format=jpg&name=4096x4096",
     comment: "23",
     repost: "90",
     like: "774",
