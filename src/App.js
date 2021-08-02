1c8fc44a3feaa152c768e0357836f70eff18650d
Olha Fastiuk <olhafastiuk@gmail.com>
2021-08-03 00:09:01 +0300

    Make tweets dynamic; Add users info

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import "./App.css";
 
 import React from "react";
 import Tweet from "./tweet";
+import Users from "./Users";
 
 function App() {
   return (
     <div className="tweets">
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
+      {Users.map(user => (
+        <Tweet img={user.img} name={user.name} nickname={user.nickname} date={user.date} post={user.post} img_post={user.img_post} comment={user.comment} repost={user.repost} like={user.like} />
+      ))}
     </div>
   );
 }
